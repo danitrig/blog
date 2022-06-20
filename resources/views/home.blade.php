a791089d4be1463625b2fa8095e67d4dbe6963b5
Daniel Gómez <danitrig@live.com>
2022-06-21 00:49:13 +0200

    Agregamos AdminLTE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+@section('title', 'Admin Zone')
+
+@section('content_header')
+    <h1>Zona de Administración del Blog</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <p>Zona del admin panel para la gestión del Blog para la TAREA 6 de Servidor.</p>
+@stop
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('Logueado Correctamente') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,11 @@ Route::get('/', function () {
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/post', [App\Http\Controllers\HomeController::class, 'post'])->name('post');
+Route::get('/home', function () {
+    return view('home');
+})->middleware('auth');
+
+Route::get('/admin/categories', [App\Http\Controllers\Admin\CategoriesController::class, 'index'])->name('admin.categories');
 
 
 Auth::routes();
